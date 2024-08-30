0830783dc9eecbf6054f779564526a2ea3ab04d5
liang LI <liangl79@gmail.com>
2024-08-30 20:19:15 +0800

    X25519 Test NG, Why? ...

--- MCU/RockeyARM/app.cc
+++ MCU/RockeyARM/app.cc
@@ -11,6 +11,8 @@ rLANG_DECLARE_END
 
 rLANGEXPORT int rLANGAPI app_entry() {
   led_control(LED_BLINK);
-  machine::dongle::Start(reinterpret_cast<void*>(0x68000000), reinterpret_cast<void*>(0x68000C00));
+  int result = machine::dongle::Start(reinterpret_cast<void*>(0x68000000), reinterpret_cast<void*>(0x68000C00));
   led_control(LED_OFF);
+
+  return result;
 }
--- src/Dongle/dongle.cc
+++ src/Dongle/dongle.cc
@@ -6,34 +6,39 @@ rLANG_DECLARE_MACHINE
 namespace Curve25519 {
 
 struct Curve25519Context {
-  uint8_t private_ed25519_az_[32];
-  uint8_t private_x25519_[32];
   uint8_t public_ed25519_[32];
   uint8_t public_x25519__[32];
+  uint8_t pubkey_exchang_[32];
+  uint8_t shared_secret__[32];
 
   uint32_t TAG_;
 };
 
 int Check(void* InOutBuf, void* ExtendBuf) {
   constexpr uint32_t TAG = rLANG_DECLARE_MAGIC_Xs("25519");
+  uint8_t private_[32];
 
   Curve25519Context* const Context = static_cast<Curve25519Context*>(InOutBuf);
-  for (int i = 0; i < 32; ++i) {
-    Context->private_ed25519_az_[i] = Context->private_x25519_[i] = static_cast<uint8_t>(i);
-  }
-  // Context->result_[0] = get_random(Context->private_ed25519_az_, sizeof(Context->private_ed25519_az_));
-  // Context->result_[1] = get_random(Context->private_x25519_, sizeof(Context->private_x25519_));
+  for (int i = 0; i < 32; ++i)
+    private_[i] = static_cast<uint8_t>(i);
 
-  rlCryptoEd25519PubkeyEx(Context->public_ed25519_, Context->private_ed25519_az_);
-  rlCryptoX25519Pubkey(Context->public_x25519__, Context->private_x25519_);
-  std::ignore = Context->TAG_ = TAG;
+  rlCryptoEd25519PubkeyEx(Context->public_ed25519_, private_);
+  rlCryptoX25519Pubkey(Context->public_x25519__, private_);
+
+  /**! */
+  rlCryptoX25519Pubkey(Context->pubkey_exchang_, Context->public_ed25519_);
+  rlCryptoX25519(Context->shared_secret__, private_, Context->pubkey_exchang_);
 
 #ifndef X_BUILD_native
   rlLOGXI(TAG, Context->public_ed25519_, sizeof(Context->public_ed25519_), "Context->public_ed25519_:");
   rlLOGXI(TAG, Context->public_x25519__, sizeof(Context->public_x25519__), "Context->public_x25519__:");
+  rlLOGXI(TAG, Context->pubkey_exchang_, sizeof(Context->pubkey_exchang_), "Context->pubkey_exchang_:");
+  rlLOGXI(TAG, Context->shared_secret__, sizeof(Context->shared_secret__), "Context->shared_secret__:");
 #endif /* X_BUILD_native */
 
-  return 0;
+  std::ignore = Context->TAG_ = TAG;
+
+  return 10086;
 }
 
 }  // namespace Curve25519
