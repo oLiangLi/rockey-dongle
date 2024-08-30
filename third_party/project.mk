1670cac440c335540e99e5ec9dbd19265fdc6fbb
liangli <liangl79@rlang.xyz>
2024-08-30 23:58:34 +0800

    add Win32 build script ...

--- third_party/project.mk
+++ third_party/project.mk
@@ -1,4 +1,5 @@
 THIRD_PARTY_INSTALL_PREFIX := $(shell mkdir -p $(X4C_GENERATE)/System && realpath $(X4C_GENERATE)/System)
+THIRD_PARTY_INSTALL_BINARY := $(shell mkdir -p $(X4C_BINARY) && realpath "$(X4C_BINARY)")
 
 ifeq ("$(X4C_BUILD)","emscripten")
 
@@ -61,3 +62,40 @@ $(BUILD_TASSL_LIBRARY_BUILD_STAMP):
 	touch $@
 
 endif ## linux build Tassl ...
+
+
+ifeq ("$(X4C_ARCH)-$(X4C_BUILD)","amd64-windows")
+
+##
+##
+##
+THIRD_PARTY_INSTALL_WIN_PREFIX := $(shell cygpath -m "$(THIRD_PARTY_INSTALL_PREFIX)")
+
+##
+##
+##
+X4C_COMMON_CFLAGS   += -I$(THIRD_PARTY_INSTALL_WIN_PREFIX)/include
+X4C_COMMON_CXXFLAGS += -I$(THIRD_PARTY_INSTALL_WIN_PREFIX)/include
+X4C_COMMON_LDFLAGS  += $(THIRD_PARTY_INSTALL_WIN_PREFIX)/lib/libcrypto.dll.a $(THIRD_PARTY_INSTALL_WIN_PREFIX)/lib/libssl.dll.a
+
+##
+##
+##
+BUILD_TASSL_LIBRARY_BUILD_ROOT   := $(THIRD_PARTY_INSTALL_PREFIX)/Build-TASSL
+BUILD_TASSL_LIBRARY_BUILD_STAMP  := $(BUILD_TASSL_LIBRARY_BUILD_ROOT)/.build-tassl-done
+BUILD_TASSL_LIBRARY_SOURCE_ROOT  := $(shell realpath $(wORLD_ROOT)/third_party/TASSL-1.1.1)
+
+prepare: build-tassl-library
+
+build-tassl-library: $(BUILD_TASSL_LIBRARY_BUILD_STAMP)
+$(BUILD_TASSL_LIBRARY_BUILD_STAMP):
+	mkdir -p $(BUILD_TASSL_LIBRARY_BUILD_ROOT)
+	cd $(BUILD_TASSL_LIBRARY_BUILD_ROOT) && $(BUILD_TASSL_LIBRARY_SOURCE_ROOT)/Configure --prefix=$(THIRD_PARTY_INSTALL_PREFIX) \
+		-no-tests mingw64 --cross-compile-prefix=x86_64-w64-mingw32- --openssldir=/tmp/jsCrypto/ssl
+	$(MAKE) -C $(BUILD_TASSL_LIBRARY_BUILD_ROOT) ENGINESDIR=/Machine/System/engine OPENSSLDIR=/Machine/System/ssl -i
+	$(MAKE) -C $(BUILD_TASSL_LIBRARY_BUILD_ROOT) install -i
+	install -m $(SO_INSTALL_MODE) $(THIRD_PARTY_INSTALL_PREFIX)/bin/libcrypto-1_1-x64.dll "$(THIRD_PARTY_INSTALL_BINARY)"
+	install -m $(SO_INSTALL_MODE) $(THIRD_PARTY_INSTALL_PREFIX)/bin/libssl-1_1-x64.dll "$(THIRD_PARTY_INSTALL_BINARY)"
+	touch $@
+
+endif ## windows build Tassl ...
