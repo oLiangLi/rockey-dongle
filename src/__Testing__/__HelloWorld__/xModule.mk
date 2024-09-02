b458f980f92a0e9ac64870c7c4af8383f32f4d5f
liangli <liangl79@rlang.xyz>
2024-09-02 09:22:59 +0800

    整理下代码 ..

--- src/__Testing__/__HelloWorld__/xModule.mk
+++ src/__Testing__/__HelloWorld__/xModule.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(my-dir)
 ifneq ("$(X4C_BUILD)","native")
 
 $(call clear-local-vars)
-LOCAL_MODULE := HelloWorld
+LOCAL_MODULE := __Testing__HelloWorld__
 
 $(call add_general_source_files_under, $(LOCAL_PATH))
 $(call module_depends, base)
