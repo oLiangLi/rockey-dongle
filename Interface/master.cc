6c7cb7ef84da7e661b2d9614bab1e777b3a66f54
LiangLI <liangl79@gmail.com>
2026-03-23 03:14:12 +0100

    impl OpManager ...

--- Interface/master.cc
+++ Interface/master.cc
@@ -13,6 +13,7 @@ int VM_t::OpManager_WorldInitialize() {
   uint8_t init[256];
   memset(init, 0, sizeof(init));
 
+  int error = 0;
   dongle_->DeleteFile(SECRET_STORAGE_TYPE::kSM2, kKeyIdGlobalSM2ECDSA);
   dongle_->DeleteFile(SECRET_STORAGE_TYPE::kSM2, kKeyIdGlobalSM2ECIES);
   dongle_->DeleteFile(SECRET_STORAGE_TYPE::kP256, kKeyIdGlobalP256ECDSA);
@@ -21,7 +22,13 @@ int VM_t::OpManager_WorldInitialize() {
   dongle_->WriteShareMemory(init);
 
   for (int off = 0; off < kSizeDashboard; off += sizeof(init)) {
-    dongle_->WriteDataFile(Dongle::kFactoryDataFileId, off, init, sizeof(init));
+    if (0 != dongle_->WriteDataFile(Dongle::kFactoryDataFileId, off, init, sizeof(init)))
+      ++error;
+  }
+
+  if (error) {
+    rlLOGE(TAG, "Initialize Factory DataFile Error %d", error);
+    return -EFAULT;
   }
 
   return 0;
@@ -180,7 +187,8 @@ int VM_t::OpManager_VerifyWorldPublic() {
 
   size = sizeof(MASTER_SECRET);
   result = dongle->SM2Decrypt(kKeyIdGlobalSM2ECDSA, MASTER_SECRET, sizeof(MASTER_SECRET), MASTER_SECRET, &size);
-  if (0 != result && size != kSize_MASTER_SECRET) {
+  memset(MASTER_SECRET, 0, sizeof(MASTER_SECRET));
+  if (0 != result || size != kSize_MASTER_SECRET) {
     rlLOGE(TAG, "kKeyIdGlobalSM2ECDSA.SM2Decrypt Failed %d, size: %zd", result, size);
     return -EFAULT;
   }
@@ -191,6 +199,58 @@ int VM_t::OpManager_VerifyWorldPublic() {
 }
 
 int VM_t::OpManager_UpdateSM2ECIESKey(uint8_t public_[64], uint8_t* private_) {
+  WorldPublic world_public_;
+  int result = dongle_->ReadDataFile(Dongle::kFactoryDataFileId, WorldPublic::kOffsetDataFile, &world_public_,
+                                     sizeof(world_public_));
+  if (0 != result)
+    return result;
+
+  result = dongle_->GenerateSM2(WorldPublic::kFileSM2ECIES, &public_[0], &public_[32], private_);
+  if (0 != result)
+    return result;
+
+  memcpy(&world_public_.dongle_sm2ecies_pubkey_[0], public_, 64);
+  result = dongle_->SM3(&world_public_, WorldPublic::kOffsetSign_SM2ECIES, &world_public_.dongle_sm2ecies_sign_[0]);
+  if (0 != result)
+    return result;
+
+  result = dongle_->SM2Sign(WorldPublic::kFileSM2ECIES, &world_public_.dongle_sm2ecies_sign_[0],
+                            &world_public_.dongle_sm2ecies_sign_[0], &world_public_.dongle_sm2ecies_sign_[32]);
+  if (0 != result)
+    return result;
+
+  result = dongle_->SHA256(&world_public_, WorldPublic::kOffsetSign_RSA2048, &world_public_.dongle_rsa2048_sign_[0]);
+  if (0 != result)
+    return result;
+
+  size_t size = 32;
+  result = dongle_->RSAPrivate(WorldPublic::kFileRSA2048, &world_public_.dongle_rsa2048_sign_[0], &size, true);
+  if (0 != result)
+    return result;
+
+  result =
+      dongle_->SHA256(&world_public_, WorldPublic::kOffsetSign_Secp256r1, &world_public_.dongle_secp256r1_sign_[0]);
+  if (0 != result)
+    return result;
+
+  result = dongle_->P256Sign(WorldPublic::kFileSECP256r1, &world_public_.dongle_secp256r1_sign_[0],
+                             &world_public_.dongle_secp256r1_sign_[0], &world_public_.dongle_secp256r1_sign_[32]);
+  if (0 != result)
+    return result;
+
+  result = dongle_->SM3(&world_public_, WorldPublic::kOffsetSign_SM2ECDSA, &world_public_.dongle_sm2ecdsa_sign_[0]);
+  if (0 != result)
+    return result;
+
+  result = dongle_->SM2Sign(WorldPublic::kFileSM2ECDSA, &world_public_.dongle_sm2ecdsa_sign_[0],
+                            &world_public_.dongle_sm2ecdsa_sign_[0], &world_public_.dongle_sm2ecdsa_sign_[32]);
+  if (0 != result)
+    return result;
+
+  result = dongle_->WriteDataFile(Dongle::kFactoryDataFileId, WorldPublic::kOffsetDataFile, &world_public_,
+                                  sizeof(world_public_));
+  if (0 != result)
+    return result;
   return 0;
 }
 
@@ -236,14 +296,15 @@ int VM_t::OpManager_ComputeSecretBytes(uint8_t bytes_[64]) {
     uint32_t world_seed_1_;
     uint8_t input_bytes_[64];
     uint32_t world_seed_2_;
-    uint8_t MASTER_SECRET[kSize_MASTER_SECRET];
-    uint32_t world_seed_3_;
-    uint8_t hash_value_[64];
+    rlCryptoShaCtx hash_context_;
     uint8_t ENCRYPTO_MASTER_SECRET[kSize_MASTER_SECRET + 96];
+    uint8_t MASTER_SECRET[kSize_MASTER_SECRET];
+    uint8_t hash_value_[64];
+    uint32_t world_seed_3_;
   };
-  Dongle::SecretBuffer<1, Context> storage_;
-  Context& MASTER_SECRET_CONTEXT = storage_[0];
 
+  int error = 0;
+  Context MASTER_SECRET_CONTEXT;
   memset(&MASTER_SECRET_CONTEXT, 0, sizeof(MASTER_SECRET_CONTEXT));
   MASTER_SECRET_CONTEXT.world_seed_0_ = rLANG_WORLD_SEED_0;
   MASTER_SECRET_CONTEXT.world_seed_1_ = rLANG_WORLD_SEED_1;
@@ -261,28 +322,91 @@ int VM_t::OpManager_ComputeSecretBytes(uint8_t bytes_[64]) {
 
   size_t size = kSize_MASTER_SECRET + 96;
   result = dongle_->SM2Decrypt(kKeyIdGlobalSM2ECDSA, MASTER_SECRET_CONTEXT.ENCRYPTO_MASTER_SECRET, size,
-                               MASTER_SECRET_CONTEXT.MASTER_SECRET, kSize_MASTER_SECRET);
+                               MASTER_SECRET_CONTEXT.MASTER_SECRET, &size);
   if (0 != result)
-    return result;
+    ++error;
 
-  for(int loop = 0; loop < 3; ++loop) {
-    MASTER_SECRET_CONTEXT.world_seed_2_ += rLANG_WORLD_SEED_0;
-    MASTER_SECRET_CONTEXT.world_seed_3_ += rLANG_WORLD_SEED_2;
-    result = dongle_->SM3(&MASTER_SECRET_CONTEXT, sizeof(MASTER_SECRET_CONTEXT), &MASTER_SECRET_CONTEXT.hash_value_[0]);
-    if (0 != result)
-      return result;
-    MASTER_SECRET_CONTEXT.world_seed_0_ += rLANG_WORLD_SEED_1;
-    MASTER_SECRET_CONTEXT.world_seed_1_ += rLANG_WORLD_SEED_3;
-    result =
-        dongle_->SHA256(&MASTER_SECRET_CONTEXT, sizeof(MASTER_SECRET_CONTEXT), &MASTER_SECRET_CONTEXT.hash_value_[32]);
-    if (0 != result)
-      return result;
-  }
+  ((Sha256Ctx*)&MASTER_SECRET_CONTEXT.hash_context_)
+      ->Init()
+      .Update(&MASTER_SECRET_CONTEXT, sizeof(MASTER_SECRET_CONTEXT))
+      .Final(&MASTER_SECRET_CONTEXT.hash_value_[32]);
 
-  return dongle_->SHA512(&MASTER_SECRET_CONTEXT, sizeof(MASTER_SECRET_CONTEXT), bytes_);
+  MASTER_SECRET_CONTEXT.world_seed_0_ += rLANG_WORLD_SEED_2;
+  MASTER_SECRET_CONTEXT.world_seed_3_ += rLANG_WORLD_SEED_1;
+
+  result = dongle_->SM3(&MASTER_SECRET_CONTEXT, sizeof(MASTER_SECRET_CONTEXT), &MASTER_SECRET_CONTEXT.hash_value_[0]);
+  if (0 != result)
+    ++error;
+
+  MASTER_SECRET_CONTEXT.world_seed_2_ += rLANG_WORLD_SEED_3;
+  MASTER_SECRET_CONTEXT.world_seed_1_ += rLANG_WORLD_SEED_0;
+
+  ((Sha512Ctx*)&MASTER_SECRET_CONTEXT.hash_context_)
+      ->Init()
+      .Update(&MASTER_SECRET_CONTEXT, sizeof(MASTER_SECRET_CONTEXT))
+      .Final(bytes_);
+  memset(&MASTER_SECRET_CONTEXT, 0, sizeof(MASTER_SECRET_CONTEXT));
+  if (error)
+    return -EFAULT;
+
+  return 0;
 }
 
 int VM_t::OpManager_ComputeEnTrustData(int argc, int32_t argv[]) {
+  if (argc != 3)
+    return zero_ = -EINVAL;
+
+  int input_length = argv[2];
+  if (input_length < 8 || input_length > 512)
+    return zero_ = -EINVAL;
+  const uint8_t* input_bytes = (const uint8_t*)OpCheckMM(argv[1], input_length);
+  if (!input_bytes)
+    return zero_ = SIGSEGV;
+
+  int output_length = 80 + input_length;
+  uint8_t* InOutBuff = (uint8_t*)OpCheckMM(argv[0], output_length);
+  if (!InOutBuff)
+    return zero_ = SIGSEGV;
+
+  auto CheckHid = [this](const uint8_t* key) {
+    /* || hid[12] | kid[3] | Zero | X[32] | Y[32] || */
+    uint8_t value = 0;
+    for (int i = 0; i < 12; ++i)
+      value |= key[i];
+    if (0 == value)
+      return 0;
+    if (key[15] != 0)
+      return -EINVAL;
+    int result = dongle_->CheckPointOnCurveSM2(&key[16], &key[48]);
+    if (0 != result)
+      return -EINVAL;
+    return 1;
+  };
+
+  int result = CheckHid(InOutBuff);
+  if (result < 0)
+    return zero_ = result;
+
+  if (0 == result) {
+    memset(InOutBuff, 0, output_length);
+    return 0;
+  }
+
+  auto* OutputBuffer = (uint8_t*)buffer_;
+  result = dongle_->SM2Encrypt(&InOutBuff[16], &InOutBuff[48], input_bytes, input_length, OutputBuffer);
+  if (0 != result)
+    return zero_ = result;
+
+  /* || hid[12] | kid[3] | Yodd | X[32] | H[32] | XOR[length] || */
+  InOutBuff[15] = OutputBuffer[63] & 1;
+  memcpy(&InOutBuff[16], &OutputBuffer[0], 32);
+  memcpy(&InOutBuff[48], &OutputBuffer[64], 32 + input_length);
+
+  result = dongle_->DecompressPointSM2(OutputBuffer, OutputBuffer, InOutBuff[15]);
+  if (0 != result)
+    return zero_ = result;
+  if (0 != memcmp(&InOutBuff[0], &InOutBuff[32], 32))
+    return zero_ = -EFAULT;
   return 0;
 }
 
@@ -293,15 +417,15 @@ int VM_t::OpManager(uint16_t op, int argc, int32_t argv[]) {
 
   cycles_ -= 64 * 1024;
   if (op == OpCode::kWorldInitialize) {
-    if(0 != argc)
+    if (0 != argc)
       return zero_ = -EINVAL;
     return zero_ = OpManager_WorldInitialize();
   } else if (op == OpCode::kVerifyWorldPublic) {
-    if(0 != argc)
+    if (0 != argc)
       return zero_ = -EINVAL;
     return zero_ = OpManager_VerifyWorldPublic();
   } else if (op == OpCode::kUpdateSM2ECIESKey) {
-    if(argc != 1 && argc != 2)
+    if (argc != 1 && argc != 2)
       return zero_ = -EINVAL;
     uint8_t* public_ = (uint8_t*)OpCheckMM(argv[0], 64);
     uint8_t* private_ = nullptr;
@@ -309,7 +433,7 @@ int VM_t::OpManager(uint16_t op, int argc, int32_t argv[]) {
       return zero_ = SIGSEGV;
     return zero_ = OpManager_UpdateSM2ECIESKey(public_, private_);
   } else if (op == OpCode::kUpdateMasterSecret) {
-    if(0 != argc)
+    if (0 != argc)
       return zero_ = -EINVAL;
     return zero_ = OpManager_UpdateMasterSecret();
   } else if (op == OpCode::kComputeSecretBytes) {
