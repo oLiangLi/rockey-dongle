c1535ba48369b9fa16164a8f3259924430d08aba
LiangLI <liangl79@gmail.com>
2026-04-03 10:31:48 +0800

    CROSS_COMPILE=aarch64-linux-gnu- ...

--- third_party/project.mk
+++ third_party/project.mk
@@ -1,4 +1,4 @@
-THIRD_PARTY_INSTALL_PREFIX := $(shell mkdir -p $(X4C_GENERATE)/System && realpath $(X4C_GENERATE)/System)
+﻿THIRD_PARTY_INSTALL_PREFIX := $(shell mkdir -p $(X4C_GENERATE)/System && realpath $(X4C_GENERATE)/System)
 THIRD_PARTY_INSTALL_BINARY := $(shell mkdir -p $(X4C_BINARY) && realpath "$(X4C_BINARY)")
 
 ifeq ("$(X4C_BUILD)","emscripten")
@@ -74,6 +74,33 @@ ifeq ("$(X4C_ARCH)-$(X4C_BUILD)","aarch64-linux")
 X4C_COMMON_CFLAGS   += -I$(wORLD_ROOT)/third_party/RockeyARM/aarch64-linux/include
 X4C_COMMON_CXXFLAGS += -I$(wORLD_ROOT)/third_party/RockeyARM/aarch64-linux/include
 X4C_COMMON_LDFLAGS  += -L$(wORLD_ROOT)/third_party/RockeyARM/aarch64-linux/lib -lRockeyARM
+
+##
+##
+##
+X4C_COMMON_CFLAGS   += -I$(THIRD_PARTY_INSTALL_PREFIX)/include
+X4C_COMMON_CXXFLAGS += -I$(THIRD_PARTY_INSTALL_PREFIX)/include
+X4C_COMMON_LDFLAGS  += -L$(THIRD_PARTY_INSTALL_PREFIX)/lib -lssl -lcrypto -ldl
+
+##
+##
+##
+.PHONY : build-tassl-library
+
+BUILD_TASSL_LIBRARY_BUILD_ROOT   := $(THIRD_PARTY_INSTALL_PREFIX)/Build-TASSL
+BUILD_TASSL_LIBRARY_BUILD_STAMP  := $(BUILD_TASSL_LIBRARY_BUILD_ROOT)/.build-tassl-done
+BUILD_TASSL_LIBRARY_SOURCE_ROOT  := $(shell realpath $(wORLD_ROOT)/third_party/TASSL-1.1.1)
+
+prepare: build-tassl-library
+
+build-tassl-library: $(BUILD_TASSL_LIBRARY_BUILD_STAMP)
+$(BUILD_TASSL_LIBRARY_BUILD_STAMP):
+	mkdir -p $(BUILD_TASSL_LIBRARY_BUILD_ROOT)
+	cd $(BUILD_TASSL_LIBRARY_BUILD_ROOT) && $(BUILD_TASSL_LIBRARY_SOURCE_ROOT)/Configure --prefix=$(THIRD_PARTY_INSTALL_PREFIX) \
+		-static -no-tests linux-aarch64 --openssldir=/tmp/jsCrypto/ssl
+	$(MAKE) -C $(BUILD_TASSL_LIBRARY_BUILD_ROOT) CROSS_COMPILE=aarch64-linux-gnu- ENGINESDIR=/Machine/System/engine OPENSSLDIR=/Machine/System/ssl -i
+	$(MAKE) -C $(BUILD_TASSL_LIBRARY_BUILD_ROOT) CROSS_COMPILE=aarch64-linux-gnu- install_sw -i
+	touch $@
 
 endif ## aarch64-linux RockeyARM ...
 
